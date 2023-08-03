9d057711469e21359cd11876532ddb8c371ec6af
SteamyCutie <eric.vale0128@gmail.com>
2023-08-03 10:24:46 -0400

    add: header page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FeatureComponent } from './feature/feature.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactusComponent,
+    TestimonialComponent,
+    FeatureComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
